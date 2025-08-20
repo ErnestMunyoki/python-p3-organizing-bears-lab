0a336360b9415bf6481973a622ced4aecc853837
ErnestMunyoki <ernestmunyoki@gmail.com>
2025-08-20 20:37:05 +0300

    changes made to sql_queries.py

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -6,9 +6,16 @@ select_all_female_bears_return_name_and_age = """
     WHERE sex = 'F';
 """
 
-select_all_bears_names_and_ages_ordered_by_age = """
+select_all_bears_names_and_orders_in_alphabetical_order = """
+    SELECT name
+    FROM bears
+    ORDER BY name ASC;
+"""
+
+select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
     SELECT name, age
     FROM bears
+    WHERE alive = 1
     ORDER BY age ASC;
 """
 
@@ -24,27 +31,4 @@ select_youngest_bear_and_returns_name_and_age = """
     FROM bears
     ORDER BY age ASC
     LIMIT 1;
-"""
-
-select_bear_with_null_name = """
-    SELECT *
-    FROM bears
-    WHERE name IS NULL;
-"""
-
-select_bear_with_name_not_null = """
-    SELECT *
-    FROM bears
-    WHERE name IS NOT NULL;
-"""
-
-select_all_bears_ordered_by_temperament = """
-    SELECT *
-    FROM bears
-    ORDER BY temperament ASC;
-"""
-select_all_bears_names_and_orders_in_alphabetical_order = """
-    SELECT name
-    FROM bears
-    ORDER BY name ASC;
 """
